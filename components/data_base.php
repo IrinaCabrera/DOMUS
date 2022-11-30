632dcaa28a125786558768083f752278b436afd3
Fernando Zalazar <zalazarfernando686@outlook.es>
2022-11-30 13:01:00 -0300

    Verify with function

--- components/data_base.php
+++ components/data_base.php
@@ -1,3 +1,25 @@
+<?php 
+    function verify_secr($conn,$row,$user_ver){
+        try{
+            $aux = $row['DNI'];
+            $sql = "SELECT DNI FROM $user_ver WHERE DNI = '$aux'";
+    
+            $result = mysqli_query($conn, $sql);
+
+            $row = mysqli_fetch_array($result);
+        } catch (mysqli_sql_exception $e) { 
+            var_dump($e);
+            exit; 
+        }
+
+        if(!empty($row['DNI'])){
+            include("./indexSecretaria.php");
+        }else{
+            include("./index.php"); 
+        }
+    }
+?>
+
 <?php
     $dbhost = "localhost";
     $dbuser = "root";
@@ -10,7 +32,7 @@
     $pwd = $_POST['exampleInputPassword1'];
 
     try{
-        $sql = "SELECT Email, Contraseña FROM usuario WHERE Email = '$email' AND Contraseña = '$pwd' ";
+        $sql = "SELECT Email, Contraseña, DNI FROM usuario WHERE Email = '$email' AND Contraseña = '$pwd' ";
 
         $result = mysqli_query($conn, $sql);
     } catch (mysqli_sql_exception $e) { 
@@ -20,10 +42,7 @@
 
     $row = mysqli_fetch_array($result);
 
-    if(empty($row['Email']) && empty($row['Contraseña'])){
-        include("./index.php");
-    }else{
-        include("./indexSecretaria.php");
+    if(!empty($row['Email']) && !empty($row['Contraseña'])){
+        verify_secr($conn ,$row,'secretario');
     }
-    
 ?>
