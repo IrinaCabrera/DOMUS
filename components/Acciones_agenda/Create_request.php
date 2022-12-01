9a63a6e9f727f7e8681765a45e661b5862e8f807
Fernando Zalazar <zalazarfernando686@outlook.es>
2022-12-01 10:18:18 -0300

    create request customer

---
+++ components/Acciones_agenda/Create_request.php
@@ -0,0 +1,12 @@
+<?php
+    include("../data_base.php");
+?>
+
+<?php
+    $asunto = $_POST[''];
+    $dni = $_POST[''];
+
+    $sql = "INSERT INTO solicitud(Asunto,DNI_cliente) VALUE($asunto,$dni)";
+
+    mysqli_query($conn, $sql);
+?>
