3c3bdd03756af37097381529e89105bb457f0293
Fernando Zalazar <zalazarfernando686@outlook.es>
2022-12-02 23:04:03 -0300

    Add fields to requests

--- components/Acciones_agenda/Create_request.php
+++ components/Acciones_agenda/Create_request.php
@@ -5,8 +5,15 @@
 <?php
     $asunto = $_POST[''];
     $dni = $_POST[''];
+    $name = $_POST[''];
+    $phone = $_POST[''];
+    $mail = $_POST[''];
+    $hour = $_POST[''];
+    $date = $_POST['']; 
 
-    $sql = "INSERT INTO solicitud(Asunto,DNI_cliente) VALUE($asunto,$dni)";
+
+    $sql = "INSERT INTO solicitud(Asunto,DNI_cliente,Nombre_cl,Telefono,Email,Hora,Fecha) 
+            VALUE($asunto,$dni,$name,$phone,$mail,$hour,$date)";
 
     mysqli_query($conn, $sql);
 ?>
--- components/Acciones_agenda/Show_requests.php
+++ components/Acciones_agenda/Show_requests.php
@@ -1,13 +1,57 @@
 <?php
-    require('../data_base.php');
+    require('./data_base.php');
 ?>
 
 <?php
-    $sql = "SELECT Asunto, DNI_cliente FROM solicitud";
+    $sql = "SELECT Asunto, DNI_cliente, Nombre_cl, Telefono, Email, Hora, Fecha
+            FROM solicitud";
 
     $result = mysqli_query($conn, $sql);
 
     while($row = mysqli_fetch_assoc($result)){
-        echo $row['Asunto']," ",$row['DNI_cliente'];
+
+        echo"<tr>";
+        echo"<th scope='row'>".$row['Asunto']." "."</th>";
+        echo"<td>".$row['DNI_cliente'].$row['Nombre_cl'].$row['Telefono'].$row['Email'].$row['Hora'].$row['Fecha']."</td>";
+        echo"<td><button type='button' class='btn btn-primary'>Realizar Cita</button>"." "."<button type='button' class='btn btn-primary'>Eliminar</button></td>";
+        echo"</tr>";
     }
+
+    /*echo"<tr>";
+    echo"<th scope='row'>xczczczczxczczczxczxczczxczxzcxzxczxczxczcxzczczxczxczxczxczczxczxczxczxczczcz</th>";
+    echo"<td>44444444</td>";
+    echo"<td><button type='button' class='btn btn-primary'>Realizar Cita</button></td>";
+    echo"<td><button type='button' class='btn btn-primary'>Eliminar</button></td>";
+    echo"</tr>";
+    echo"<tr>";
+    echo"<th scope='row'>xczczczczxczczczxczxczczxczxzcxzxczxczxczcxzczczxczxczxczxczczxczxczxczxczczcz</th>";
+    echo"<td>44444444</td>";
+    echo"<td><button type='button' class='btn btn-primary'>Realizar Cita</button></td>";
+    echo"<td><button type='button' class='btn btn-primary'>Eliminar</button></td>";
+    echo"</tr>";
+    echo"<tr>";
+    echo"<th scope='row'>xczczczczxczczczxczxczczxczxzcxzxczxczxczcxzczczxczxczxczxczczxczxczxczxczczcz</th>";
+    echo"<td>44444444</td>";
+    echo"<td><button type='button' class='btn btn-primary'>Realizar Cita</button></td>";
+    echo"<td><button type='button' class='btn btn-primary'>Eliminar</button></td>";
+    echo"</tr>";
+    echo"<tr>";
+    echo"<th scope='row'>xczczczczxczczczxczxczczxczxzcxzxczxczxczcxzczczxczxczxczxczczxczxczxczxczczcz</th>";
+    echo"<td>44444444</td>";
+    echo"<td><button type='button' class='btn btn-primary'>Realizar Cita</button></td>";
+    echo"<td><button type='button' class='btn btn-primary'>Eliminar</button></td>";
+    echo"</tr>";
+    echo"<tr>";
+    echo"<th scope='row'>xczczczczxczczczxczxczczxczxzcxzxczxczxczcxzczczxczxczxczxczczxczxczxczxczczcz</th>";
+    echo"<td>44444444</td>";
+    echo"<td><button type='button' class='btn btn-primary'>Realizar Cita</button></td>";
+    echo"<td><button type='button' class='btn btn-primary'>Eliminar</button></td>";
+    echo"</tr>";
+    echo"<tr>";
+    echo"<th scope='row'>xczczczczxczczczxczxczczxczxzcxzxczxczxczcxzczczxczxczxczxczczxczxczxczxczczcz</th>";
+    echo"<td>44444444</td>";
+    echo"<td><button type='button' class='btn btn-primary'>Realizar Cita</button></td>";
+    echo"<td><button type='button' class='btn btn-primary'>Eliminar</button></td>";
+    echo"</tr>";*/
+        
 ?>
